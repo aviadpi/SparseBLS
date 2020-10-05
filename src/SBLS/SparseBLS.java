4cb8dcbba1528c6a0facf48b05db2d55ba211960
aviadpi <aviadpi@10.0.0.10>
2020-10-05 15:50:32 +0300

    Switched bwetween i & j, to be consistent with the paper. Moved duration calculation after minimal n_IT points condition.

--- src/SBLS/SparseBLS.java
+++ src/SBLS/SparseBLS.java
@@ -23,7 +23,7 @@ public class SparseBLS extends ParametersAndMethods {
 	 */
 	private static SBLS_Results SparseBLSmethod(double[][] DataOriginal, double fStep, boolean isFlux, boolean usePrevOrder){
 		// Variables & constants declarations
-		int i2, j2, transitDurationInPoints, i1ln, j1ln, j2rn, j1best=0, j2best=0;
+		int j2, i2, transitDurationInPoints, j1ln, i1ln, i2rn, i1best=0, i2best=0;
 		double P, s, r, sMinPrev, rMinPrev, SR, SRmax, TOP_SRmax, transitDurationInTime, maxTransitDuration=0., ingress, egress;
 
 		// Create centered lightcurve with normalized weights
@@ -65,68 +65,68 @@ public class SparseBLS extends ParametersAndMethods {
 			} catch (IndexOutOfBoundsException e) {
 				e.printStackTrace();
 			}
-			int j2MinPrev = 0;
+			int i2MinPrev = 0;
 			s = r = sMinPrev = rMinPrev = 0.0;
 
 			// i1 Loop - Iterate over all possible i1 values
-			for (int j1 = 0; j1 < N; j1++) {
-				int i1 	= (int) phasesIndices[j1][iIndex];
+			for (int i1 = 0; i1 < N; i1++) {
+				int j1 	= (int) phasesIndices[i1][iIndex];
 
 				// Load previous indices
-				j2 = j2MinPrev;
-				i2 = (int) phasesIndices[j2][iIndex];
+				i2 = i2MinPrev;
+				j2 = (int) phasesIndices[i2][iIndex];
 				s = sMinPrev;
 				r = rMinPrev;
-				j1ln = iMod(j1-1,N);
-				i1ln = (int) phasesIndices[j1ln][iIndex];
+				i1ln = iMod(i1-1,N);
+				j1ln = (int) phasesIndices[i1ln][iIndex];
 				
-				if (j1 == 0) {
-					ingress =  ((phasesIndices[j1ln][iTime]-1.) + phasesIndices[j1][iTime])/2.;
+				if (i1 == 0) {
+					ingress =  ((phasesIndices[i1ln][iTime]-1.) + phasesIndices[i1][iTime])/2.;
 				} else {
 					// Remove values from each end
-					s -= (Data[i1ln][iValue] * Data[i1ln][iWeight]);
-					r -= Data[i1ln][iWeight];
-					s -= (Data[i2][iValue] * Data[i2][iWeight]);
-					r -= Data[i2][iWeight];
-					ingress = (phasesIndices[j1ln][iTime] + phasesIndices[j1][iTime])/2.;
+					s -= (Data[j1ln][iValue] * Data[j1ln][iWeight]);
+					r -= Data[j1ln][iWeight];
+					s -= (Data[j2][iValue] * Data[j2][iWeight]);
+					r -= Data[j2][iWeight];
+					ingress = (phasesIndices[i1ln][iTime] + phasesIndices[i1][iTime])/2.;
 				}
 								
 				// i2 Loop - Look for relevant i2 indices
 				while (true) {
 					// Calculate transit duration in time and in points
-					j2rn = (j2 + 1) % N; // j2 right neighbor
-					
-					egress  = (phasesIndices[j2][iTime] + phasesIndices[j2rn][iTime]) / 2.;
-					if (j2rn<j2) egress += 0.5;
-					
-					transitDurationInTime = dMod(egress-ingress, 1.)*P;
-					transitDurationInPoints = j2 - j1 + 1;
-
+					transitDurationInPoints = i2 - i1 + 1;
 					// Adjust in case transit falls between folds
-					if (j2 < j1) transitDurationInPoints += N;				
-
+					if (i2 < i1) transitDurationInPoints += N;				
 					// Stoppage conditions
 					if ((transitDurationInPoints < minPointsInTransit)){
 						// Sum s,r
-						s += (Data[i2][iValue] * Data[i2][iWeight]);
-						r += Data[i2][iWeight];
-
+						s += (Data[j2][iValue] * Data[j2][iWeight]);
+						r += Data[j2][iWeight];
+						
 						// Save data
-						j2MinPrev = j2;
+						i2MinPrev = i2;
 						sMinPrev = s;
 						rMinPrev = r;
-
-						j2 = (j2 + 1) % N;
-						i2 = (int) phasesIndices[j2][iIndex];
+						
+						i2 = (i2 + 1) % N;
+						j2 = (int) phasesIndices[i2][iIndex];
 						continue;
 					}
+					
+					i2rn = (i2 + 1) % N; // j2 right neighbor
+					
+					egress  = (phasesIndices[i2][iTime] + phasesIndices[i2rn][iTime]) / 2.;
+					if (i2rn<i2) egress += 0.5;
+					
+					transitDurationInTime = dMod(egress-ingress, 1.)*P;
+
 
 					// Stoppage condition - Keplerian duration too long
 					maxTransitDuration = ParametersAndMethods.durationFactor * FastMath.pow(P,1./3.) + EPS; // Maximal transit duration up to 0.15*P=^0.333 days
 					if (transitDurationInTime > maxTransitDuration) break;
 
-					s += (Data[i2][iValue] * Data[i2][iWeight]);
-					r += Data[i2][iWeight];
+					s += (Data[j2][iValue] * Data[j2][iWeight]);
+					r += Data[j2][iWeight];
 
 					// For each i1,i2 calculate SR and update if maximal
 					double depth = s / (r * (r - 1));
@@ -135,12 +135,12 @@ public class SparseBLS extends ParametersAndMethods {
 
 					if ((SR > (SRmax + EPS)) && bigEnoughTransit){
 						SRmax = SR;
-						j1best = j1;
-						j2best = j2;
+						i1best = i1;
+						i2best = i2;
 					}
 					// Allow transit to fall between folds
-					j2 = (j2 + 1) % N;
-					i2 = (int) phasesIndices[j2][iIndex];
+					i2 = (i2 + 1) % N;
+					j2 = (int) phasesIndices[i2][iIndex];
 				} // end of i2
 			} // end of i1
 
@@ -153,18 +153,18 @@ public class SparseBLS extends ParametersAndMethods {
 				// Update InTransit indices
 				int iCurrent;
 				proposedInTransitIndices.clear();
-				if (j1best<j2best){
-					for (int j=j1best; j <= j2best; j++){
-						iCurrent = (int) phasesIndices[j][iIndex];
+				if (i1best<i2best){
+					for (int q=i1best; q <= i2best; q++){
+						iCurrent = (int) phasesIndices[q][iIndex];
 						proposedInTransitIndices.set(iCurrent);
 					}
-				}else if (j1best>j2best){
-					for (int j=0; j <= j2best; j++){
-						iCurrent = (int) phasesIndices[j][iIndex];
+				}else if (i1best>i2best){
+					for (int q=0; q <= i2best; q++){
+						iCurrent = (int) phasesIndices[q][iIndex];
 						proposedInTransitIndices.set(iCurrent);
 					}
-					for (int j=j1best; j < N; j++){
-						iCurrent = (int) phasesIndices[j][iIndex];
+					for (int q=i1best; q < N; q++){
+						iCurrent = (int) phasesIndices[q][iIndex];
 						proposedInTransitIndices.set(iCurrent);
 					}
 				}
